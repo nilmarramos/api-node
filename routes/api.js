c7e2917c4048c930d859e43e7facf85a0dbe98b1
nilmar ramos <nilmar-ramos22@hotmail.com>
2016-10-13 10:15:43 -0300

    enable cors

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@
 module.exports = function(app){
     let apiController = app.controllers.api;
 
-    app.get('/api', apiController.listar);
+    app.get('/api',  apiController.listar);
     app.get('/api/:id', apiController.consultar);
     app.post('/api', apiController.cadastrar);
     app.put('/api/:id', apiController.alterar);
